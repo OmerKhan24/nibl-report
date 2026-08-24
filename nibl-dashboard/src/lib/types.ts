f02e988a575f1be0044aea33b0ab1cb194619227
Muhammad Omer Khan <140423734+OmerKhan24@users.noreply.github.com>
2026-08-24 13:51:23 +0500

    Add files via upload

--- nibl-dashboard/src/lib/types.ts
+++ nibl-dashboard/src/lib/types.ts
@@ -109,6 +109,7 @@ export interface InvoicesApiResponse {
   collectionRate: number;
   returnsAmount: number;
   returnsCount: number;
+  pnlRevenue: number;
   outstandingCustomers: OutstandingCustomer[];
 }
 
